67a6a88ea9b554621bdcb90292ecf6180c65d354
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-04 16:11:32 +0700

    +Fix bug run app Android Java 11 -> Java 17

---
+++ src/screens/Manage/ManageScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ManageScreen = () => {
+  return (
+    <View>
+      <Text>ManageScreen</Text>
+    </View>
+  );
+};
+
+export default ManageScreen;
--- src/screens/Support/SupportScreen.tsx
+++ src/screens/Support/SupportScreen.tsx
@@ -1,11 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-const App = () => {
+
+const SupportScreen = () => {
   return (
     <View>
-      <Text>App</Text>
+      <Text>SupportScreen</Text>
     </View>
   );
 };
 
-export default App;
+export default SupportScreen;
