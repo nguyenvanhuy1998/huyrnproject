6f184ceacb395dc5f92916b55c4a41fbc9e05318
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-06 11:24:51 +0700

    Setting the header title

--- App.tsx
+++ App.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
-import {createDrawerNavigator} from '@react-navigation/drawer';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen, ProfileScreen, SettingsScreen} from './src/screens';
+import {HomeScreen} from './src/screens';
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
 
-const Root = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Profile" component={ProfileScreen} />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-    </Stack.Navigator>
-  );
-};
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Root">
-        <Drawer.Screen name="Root" component={Root} />
-        <Drawer.Screen name="Home" component={HomeScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'My home'}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
