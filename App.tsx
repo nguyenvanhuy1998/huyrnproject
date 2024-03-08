35541eaf84c78372585e1bb5337ab7a06c8df6db
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-08 13:57:14 +0700

    Sharing common options across screens

--- App.tsx
+++ App.tsx
@@ -8,16 +8,21 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{
             title: 'My home',
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
           }}
         />
       </Stack.Navigator>
