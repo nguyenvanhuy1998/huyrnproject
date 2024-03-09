c334e4540f78774caea78262ca810dab114f5091
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-09 14:47:05 +0700

    React Navigation lifecycle events

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,7 +1,14 @@
 import {View, Text, Button, StyleSheet} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const ProfileScreen = ({navigation}) => {
+  useEffect(() => {
+    return navigation.addListener('focus', () => alert('Screen was focused'));
+  }, []);
+  useEffect(() => {
+    return navigation.addListener('blur', () => alert('Screen was unfocused'));
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Profile Screen</Text>
