58d74d6bc75c45b858b9fd194fc0bab16d8866dc
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-12 10:04:38 +0700

    Hidden/Custom Header or Tab Bar

--- App.tsx
+++ App.tsx
@@ -1,92 +1,59 @@
-/* eslint-disable react/no-unstable-nested-components */
-import React, {createContext, useEffect, useMemo, useReducer} from 'react';
+import {View, Text} from 'react-native';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {HomeScreen, SignInScreen, SplashScreen} from './src/screens';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
 const Stack = createNativeStackNavigator();
-export const AuthContext = createContext();
-const App = () => {
-  const [state, dispatch] = useReducer(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'RESTORE_TOKEN':
-          return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-          };
-        case 'SIGN_IN':
-          return {
-            ...prevState,
-            isSignOut: false,
-            userToken: action.token,
-          };
-        case 'SIGN_OUT': {
-          return {
-            ...prevState,
-            isSignOut: true,
-            userToken: null,
-          };
-        }
-        default:
-          break;
-      }
-    },
-    {
-      isLoading: true,
-      isSignOut: false,
-      userToken: null,
-    },
-  );
-  useEffect(() => {
-    const bootstrapAsync = async () => {
-      let userToken;
-      try {
-        // Restore token stored in `SecureStore` or any other encrypted storage
-        // userToken = await SecureStore.getItemAsync('userToken');
-      } catch (error) {}
-      // After restoring token, we may need to validate it in production apps
-
-      // This will switch to the App screen or Auth screen and this loading
-      // screen will be unmounted and thrown away.
-      dispatch({type: 'RESTORE_TOKEN', token: userToken});
-    };
-
-    bootstrapAsync();
-  }, []);
-
-  const authContext = useMemo(() => {
-    return {
-      signIn: async data => {
-        dispatch({type: 'SIGN_IN', token: 'dummy-auth-token'});
-      },
-      signOut: () => dispatch({type: 'SIGN_OUT'}),
-      signUp: async data => {
-        dispatch({type: 'SIGN_UP', token: 'dummy-auth-token'});
-      },
-    };
-  }, []);
+const Tab = createBottomTabNavigator();
+function Demo() {
+  const insets = useSafeAreaInsets();
   return (
-    <AuthContext.Provider value={authContext}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }}>
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
+    </View>
+  );
+}
+const App = () => {
+  return (
+    <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          {state.isLoading ? (
-            <Stack.Screen name="Splash" component={SplashScreen} />
-          ) : state.userToken == null ? (
-            <Stack.Screen
-              name="SignIn"
-              component={SignInScreen}
-              options={{
-                title: 'Sign in',
-                animationTypeForReplace: state.isSignOut ? 'pop' : 'push',
-              }}
-            />
-          ) : (
-            <Stack.Screen name="Home" component={HomeScreen} />
-          )}
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Home">
+            {() => (
+              <Tab.Navigator
+                initialRouteName="Analitics"
+                screenOptions={{
+                  headerShown: false,
+                }}
+                tabBar={() => null}>
+                <Tab.Screen name="Analitics" component={Demo} />
+                <Tab.Screen name="Profile" component={Demo} />
+              </Tab.Navigator>
+            )}
+          </Stack.Screen>
+          <Stack.Screen name="Settings" component={Demo} />
         </Stack.Navigator>
       </NavigationContainer>
-    </AuthContext.Provider>
+    </SafeAreaProvider>
   );
 };
 
