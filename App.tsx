7d95559cc7be570145f3b73c1c627b4a844b8ab3
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-20 22:04:36 +0700

    +Update flow signup

--- App.tsx
+++ App.tsx
@@ -1,41 +1,22 @@
-import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {useAsyncStorage} from '@react-native-async-storage/async-storage';
-import AuthNavigator from './src/navigators/AuthNavigator';
-import {SplashScreen} from './src/screens';
+import React from 'react';
 import {StatusBar} from 'react-native';
-import {COLORS} from './src/constants';
-import MainNavigator from './src/navigators/MainNavigator';
 import {Provider} from 'react-redux';
-import store from './src/redux/store';
+import {COLORS} from './src/constants';
 import RootNavigator from './src/navigators/RootNavigator';
+import store from './src/redux/store';
 const App = () => {
-  const [isShowSplash, setIsShowSplash] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setIsShowSplash(false);
-    }, 1500);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
-    <>
+    <Provider store={store}>
       <StatusBar
         barStyle={'dark-content'}
         backgroundColor={COLORS.transparent}
         translucent
       />
-      <Provider store={store}>
-        {isShowSplash ? (
-          <SplashScreen />
-        ) : (
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
-        )}
-      </Provider>
-    </>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
--- src/navigators/RootNavigator.tsx
+++ src/navigators/RootNavigator.tsx
@@ -1,14 +1,24 @@
 import {useAsyncStorage} from '@react-native-async-storage/async-storage';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {addAuth, authSelector} from '../redux/reducers/authReducer';
 import AuthNavigator from './AuthNavigator';
 import MainNavigator from './MainNavigator';
+import {SplashScreen} from '../screens';
 
 const RootNavigator = () => {
   const {getItem} = useAsyncStorage('auth');
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
   const auth = useSelector(authSelector);
   const dispatch = useDispatch();
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
+  }, []);
   useEffect(() => {
     const checkLogin = async () => {
       const res = await getItem();
@@ -17,7 +27,13 @@ const RootNavigator = () => {
     checkLogin();
   }, [getItem, dispatch]);
 
-  return auth.accessToken ? <MainNavigator /> : <AuthNavigator />;
+  return isShowSplash ? (
+    <SplashScreen />
+  ) : auth.accessToken ? (
+    <MainNavigator />
+  ) : (
+    <AuthNavigator />
+  );
 };
 
 export default RootNavigator;
