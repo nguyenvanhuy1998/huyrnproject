402e986fd91976fea0da8a7926411d198f5c4911
huy.nguyenvan <huy.nguyenvan@mafc.com.vn>
2024-03-22 23:23:41 +0700

    Handle flow forgot password

--- src/screens/auth/ForgotPasswordScreen.tsx
+++ src/screens/auth/ForgotPasswordScreen.tsx
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, Alert} from 'react-native';
 import React, {useState} from 'react';
 import {
   ButtonComponent,
@@ -10,13 +10,41 @@ import {
 } from '../../components';
 import {ArrowRight, Sms} from 'iconsax-react-native';
 import {COLORS} from '../../constants';
+import {Validate} from '../../utils/validate';
+import authApi from '../../api/authApi';
+import {LoadingModal} from '../../modals';
 
 const ForgotPasswordScreen = () => {
   const [email, setEmail] = useState('');
+  const [disabled, setDisabled] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleCheckEmail = () => {
+    const isValidateEmail = Validate.email(email);
+    setDisabled(!isValidateEmail);
+  };
+  const handleForgotPassword = async () => {
+    setIsLoading(true);
+    try {
+      const res = await authApi.HandleAuthentication(
+        '/forgotPassword',
+        {
+          email,
+        },
+        'post',
+      );
+      console.log(res);
+      setIsLoading(false);
+      Alert.alert('Send mail', 'We sended email includes new password');
+    } catch (error) {
+      setIsLoading(false);
+      console.log('Cannot create new password api forgot password', error);
+    }
+  };
   return (
     <ContainerComponent isImageBackground back>
       <SectionComponent>
         <TextComponent text="Reset Password" title />
+        <SpaceComponent height={12} />
         <TextComponent text="Please enter your email address to request a password reset" />
         <SpaceComponent height={26} />
         <InputComponent
@@ -24,16 +52,20 @@ const ForgotPasswordScreen = () => {
           value={email}
           onChangeText={val => setEmail(val)}
           prefix={<Sms size={20} color={COLORS.gray} />}
+          onEnd={handleCheckEmail}
         />
       </SectionComponent>
       <SectionComponent>
         <ButtonComponent
+          disabled={disabled}
           text="Send"
           type="primary"
           icon={<ArrowRight size={20} color={COLORS.white} />}
           iconFlex="right"
+          onPress={handleForgotPassword}
         />
       </SectionComponent>
+      <LoadingModal visible={isLoading} />
     </ContainerComponent>
   );
 };
